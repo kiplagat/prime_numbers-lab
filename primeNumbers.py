5178292b6f2e46f76faa74809f27ec453bfa1581
Eric Kiplagat <ekiplagat@SamaDc.corp>
2017-10-13 22:25:07 +0300

    progress

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,16 @@
-
+def primeNumbers(n):
+	prime_numbers = []
+	if n < 1:
+		return []
+	if isinstance(n, list):
+		print("cannot be a list") 
+	if isinstance(n, dict):
+		print("cannot be dictionary") 
+	else:
+		for x in range(2,n):
+			for i in range(2,x):
+				if(x%i) == 0:
+					break
+			else:
+				prime_numbers.append(x)
+	return prime_numbers
